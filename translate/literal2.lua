9213cb824252a94ba647c516157dae9c49fe7a29
Ezequiel <ezeq.cruel@gmail.com>
2023-03-06 16:27:09 -0300

    Add files via upload

---
+++ translate/literal2.lua
@@ -0,0 +1,33 @@
+files = {}
+literals = {}
+
+pfile = io.popen('ls -a ../src')
+for lin in pfile:lines() do
+  if string.find(lin, '^gui.*%.c$') then
+    files[#files + 1] = lin
+  end
+end
+
+dofile('pt_BR.lua')
+
+out = io.open('en_GB.lua', 'w+')
+out:write ('translate = {}\n')
+
+for _, file in ipairs(files) do
+  out:write('--------- ' .. file .. ' --------- \n')
+  for lin in io.lines('../src/' .. file) do
+    for w in string.gmatch(lin, '_l%("(.+)"%)') do
+      if not literals[w] then
+        --out:write('translate["' ..  w .. '"] = "' .. w .. '"\n')
+        literals[w] = true
+        if translate[w] then
+          out:write('translate["' ..  w .. '"] = "' .. translate[w] .. '"\n')
+        else
+          out:write('translate["' ..  w .. '"] = "????"\n')
+        end
+      end
+    end
+  end
+end
+
+out:close()
