bcd516e604eb71408ba5ddc7fb4f3f1599aa161b
Ezequiel <ezeq.cruel@gmail.com>
2023-04-10 16:58:13 -0300

    Rename czd.bat to img/czd.bat

--- czd.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd C:\util\msys64\mingw64\bin\
-gdb.exe C:\util\CadZinho-opengl\cadzinho.exe
---
+++ img/czd.bat
@@ -0,0 +1,3 @@
+@echo off
+cd C:\util\msys64\mingw64\bin\
+gdb.exe C:\util\CadZinho-opengl\cadzinho.exe
